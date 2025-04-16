cf66a007ecaf48120a14c0963d9413f4a2ac3dda
MD Hridoy Khan <mdhridoy31121997@gmail.com>
2025-04-16 22:51:37 +0600

    all loop

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// // for loop //5bar execute korbe
+// for (let count = 1;count <= 5; count ++  ){
+//     console.log("Hridoy ke 5 bar count kora hobe");
+// }
+
+// //--------Calculate sum of 1 to 5
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//     sum = sum + i;
+//     console.log(i + " যোগ করলে মোট =", sum);// ei niom ye hoiche 1 যোগ করলে মোট = 1  2 যোগ করলে মোট = 3 3 যোগ করলে মোট = 6    
+// }
+// console.log("শেষে ফলাফল =", sum);
+
+// // ------------while loop start
+// let i = 1;
+// while ( i <= 5){
+//     console.log("i=",i);
+//     i++;
+// }
+
+
+// let j = 1;
+// while (i <= 10){
+//     console.log("Apna College");
+//     i++;
+// } 
+
+
+// ------------do while loop
+
+let i = 1;
+do {
+    console.log("do while loop");
+    i++;
+} while (i<= 5);
+
+
+// ------------for of loop
+let numbers = [10, 20, 30, 40, 50];
+
+for (let num of numbers) {
+  console.log(num);
+}
